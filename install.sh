9062795bfaa38aa461f323af39533ee76e50b818
Carl Johan Schedvin <carl.johan.schedvin@dlaboratory.com>
2023-09-10 12:25:20 +0200

    feat: add docker, node and terminal roles

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
 set -e
+export ANSIBLE_NOCOWS=1
 
-sudo apt install ansible -y
+DOTFILES_DIR="$HOME/dotfiles"
+
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo apt install -y ansible
+fi
 
 export PATH="$PATH:~/.local/bin"
 
-if [ ! -f "__bootstraped__" ]; then
+force=0
+
+while getopts 'fh' opt; do
+  case "$opt" in
+    f)
+      force=1
+      ;;
+    h)
+      echo "Usage: $(basename $0) [-f]"
+      echo "  -f: Force bootstrap"
+      exit 0
+      ;;
+    ?)
+      echo "Usage: $(basename $0) [-f]"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ ! -f "__bootstraped__" || $force -eq 1 ]]; then
   ansible-playbook --ask-become-pass ~/dotfiles/bootstrap.yml
   touch __bootstraped__
-fi
-
-if [ -f "__bootstraped__" ]; then
-  ansible-galaxy install -r requirements.yml
 fi
